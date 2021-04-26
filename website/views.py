cc9f1cabf999ad64baacbd1b4ca57fe53c3afca2
“Hei5enb3rgur” <himanshubenagi17@gmail.com>
2021-04-26 11:42:08 +0200

    Created and added database and also html files

--- website/views.py
+++ website/views.py
@@ -1,7 +1,8 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
 
 views = Blueprint('views', __name__)
 
+
 @views.route('/')
 def home_page():
-    return "<h1>Hello, Bitches</h1>"
+    return render_template('home.html', user='himanshu')
